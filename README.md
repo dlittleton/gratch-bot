ea73a0358fc9f4c14b8e7398e7183fe122577de9
Duane Littleton <dlittleton87@gmail.com>
2020-01-24 21:45:23 -0500

    Basic IR decoding
    This feels rather slow based on the servo panning speed. Especially so if
    the LEDs are lit as that can cause many decode failures to occur.
    
    Need to determine if there are any other options I can use in
    adafruit_irremote to reduce the impact.

--- README.md
+++ README.md
@@ -12,6 +12,7 @@
 
 - CircuitPython 4.1.2
 - Additional CircuitPython Libraries
+  - `adafruit_irremote`
   - `adafruit_motor`
 
 ## Resources
--- code.py
+++ code.py
@@ -4,7 +4,11 @@ import pulseio
 import time
 
 from adafruit_circuitplayground import cp
+from adafruit_irremote import GenericDecode, IRNECRepeatException
 from adafruit_motor import servo
+
+ir_pulses = pulseio.PulseIn(board.IR_RX, maxlen=200, idle_state=True)
+ir_decoder = GenericDecode()
 
 laser = digitalio.DigitalInOut(board.A3)
 laser.direction = digitalio.Direction.OUTPUT
@@ -13,23 +17,45 @@ laser.value = False
 cp.pixels.brightness = 0.1
 led_off = (0, 0, 0)
 led_purple = (50, 0, 50)
+led_red = (255, 0, 0)
+cp.pixels.fill(led_off)
 
 pwm_pan = pulseio.PWMOut(board.A1, duty_cycle=2**15, frequency=50)
 pan = servo.Servo(pwm_pan)
 
 print('Rotating')
+fails = 0
+
+def try_decode_ir():
+    global fails
+    pulse = ir_decoder.read_pulses(ir_pulses, pulse_window=0.01, blocking=False)
+    if pulse:
+        try:
+            value = ir_decoder.decode_bits(pulse)
+            print(value)
+            print(value[0] ^ value[1])
+            print(value[2] ^ value[3])
+        except IRNECRepeatException:
+            print('Repeat!')
+        except:
+            fails += 1
+            if not fails % 10:
+                print('Failures: ', fails)
+            pass
 
 while(True):
     laser.value = True
     for angle in range(0, 180, 5):
-        cp.pixels.fill(led_off)
-        cp.pixels[(angle - 1) // 18] = led_purple
+        try_decode_ir()
+        #cp.pixels.fill(led_off)
+        #cp.pixels[(angle - 1) // 18] = led_purple
         pan.angle = angle
         time.sleep(0.05)
       
     laser.value = False  
     for angle in range(180, 0, -5):
-        cp.pixels.fill(led_off)
-        cp.pixels[(angle - 1) // 18] = led_purple
+        try_decode_ir()
+        #cp.pixels.fill(led_off)
+        #cp.pixels[(angle - 1) // 18] = led_purple
         pan.angle = angle
         time.sleep(0.05)
