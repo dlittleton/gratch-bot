906bca16f534376a6d4481193c385ab91b2d61d1
Duane Littleton <dlittleton87@gmail.com>
2020-01-26 17:14:51 -0500

    Save positions and replay back at random

--- src/code.py
+++ src/code.py
@@ -5,33 +5,109 @@ Useful for determining which codes map to buttons on a remote.
 
 import board
 import pulseio
+import random
 import time
+
+from adafruit_circuitplayground import cp
 
 from remote import NecRemote
 from settings import Button, Pin
 from turret import Turret
+
+LED_GREEN = (0, 200, 0)
+LED_OFF = (0, 0, 0)
+cp.pixels.brightness = 0.01
 
 ir_pulses = pulseio.PulseIn(board.IR_RX, maxlen=500, idle_state=True)
 remote = NecRemote(ir_pulses)
 
 laser_turret = Turret(Pin.PAN, Pin.TILT, Pin.LASER)
 
-print('Receiving IR')
-target = 90
+positions = []
+
+def save_position():
+    if len(positions) < 10:
+        cp.play_tone(500, 0.25)
+        cp.play_tone(600, 0.25)
+        positions.append((laser_turret.pan, laser_turret.tilt))
+        
+    for i in range(len(positions)):
+        cp.pixels[i] = LED_GREEN
+        
+
+def clear_positions():
+    positions.clear()
+    cp.play_tone(500, 0.25)
+    cp.play_tone(400, 0.25)
+    cp.pixels.fill(LED_OFF)
+    
+    
+class State:
+    Idle = 0
+    Move = 1
+    Delay = 2
+    Firing = 3
+    
+state = State.Idle
+next_state = State.Idle
+target = 0
+
+cp.play_file('resources/ready.wav')
 while True:
     button = remote.poll()
-    if button is not None:
-        print(button)
-
-    if button == Button.Right:
-        target -= 5
-        target = max(target, 0)
-        laser_turret.pan.angle = target
+    
+    # Hackety hack, not buttons while running.
+    # REFACTOR!
+    if state != State.Idle and button != Button.StopMode:
+        button = None
+   
+    # Press Buttons
+    if button == Button.Up:
+        laser_turret.tilt += 5
+    elif button == Button.Right:
+        laser_turret.pan -= 5
+    elif button == Button.Down:
+        laser_turret.tilt -= 5
     elif button == Button.Left:
-        target += 5
-        target = min(target, 180)
-        laser_turret.pan.angle = target        
+        laser_turret.pan += 5
     elif button == Button.PlayPause:
-        laser_turret.laser.value = not laser_turret.laser.value
+        laser_turret.toggle_laser()
         remote.clear_last_button()
+    elif button == Button.Enter:
+        save_position()
+        remote.clear_last_button()
+    elif button == Button.StopMode:
+        remote.clear_last_button()
+        if state == State.Idle and positions:
+            cp.play_file('resources/firing.wav')
+            state = State.Move
+        else:
+            state = State.Idle
+            cp.play_tone(500, .25)
+            cp.play_tone(400, .25)
+    elif button == Button.Back:
+        clear_positions()
+        remote.clear_last_button()
+        
+        
+    if state == State.Move:
+        laser_turret.stop()
+        
+        pos = random.choice(positions)
+        laser_turret.pan = pos[0]
+        laser_turret.tilt = pos[1]
+        
+        target = time.monotonic() + random.randrange(1, 5)
+        state = State.Delay
+        next_state = State.Firing
+    elif state == State.Firing:
+        laser_turret.fire()
+        cp.play_tone(500, .5)
+        
+        target = time.monotonic() + random.randrange(4, 10)
+        state = State.Delay
+        next_state = State.Move
+    elif state == State.Delay:
+        if time.monotonic() > target:
+            state = next_state
 
--- src/turret.py
+++ src/turret.py
@@ -5,22 +5,71 @@ from adafruit_motor import servo
 
 def create_pwm(pin):
     return pulseio.PWMOut(pin, duty_cycle=2**15, frequency=50)
-    
+
 
 def create_output(pin):
     out = digitalio.DigitalInOut(pin)
     out.direction = digitalio.Direction.OUTPUT
     return out
+    
+    
+def clamp(v, vmin, vmax):
+    if v < vmin:
+        return vmin
+    elif v > vmax:
+        return vmax
+    else:
+        return v
+
 
 class Turret:
     ''' Laser Turret Control '''
-    
+
     def __init__(self, pan, tilt, laser):
-        self.pan = servo.Servo(create_pwm(pan))
-        self.tilt = servo.Servo(create_pwm(tilt))
+        self._pan_servo = servo.Servo(create_pwm(pan))
+        self._tilt_servo = servo.Servo(create_pwm(tilt))
+
+        self._laser = create_output(laser)
+
+        self._pan_target = 90
+        self._tilt_target = 90
         
-        self.laser = create_output(laser)
+        self._pan_servo.angle = self._pan_target
+        self._tilt_servo.angle = self._tilt_target
         
-        self.pan.angle = 90
-        self.tilt.angle = 90
+    
+    @property
+    def pan(self):
+        return self._pan_target
+        
+    
+    @pan.setter
+    def pan(self, value):
+        v = clamp(value, 0, 180)
+        self._pan_target = v
+        self._pan_servo.angle = v
+
+    
+    @property
+    def tilt(self):
+        return self._tilt_target
+        
+    
+    @tilt.setter
+    def tilt(self, value):
+        v = clamp(value, 90, 180)
+        self._tilt_target = v
+        self._tilt_servo.angle = v
+        
+    
+    def toggle_laser(self):
+        self._laser.value = not self._laser.value
+        
+        
+    def fire(self):
+        self._laser.value = True
+        
+        
+    def stop(self):
+        self._laser.value = False
         
